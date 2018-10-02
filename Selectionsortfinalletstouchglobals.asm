0a7461844e671885c21902ca5558d8808625cb12
Nick Stone <35820605+Stonen2@users.noreply.github.com>
2018-10-01 23:19:09 -0400

    Add files via upload

--- Selectionsortfinalletstouchglobals.asm
+++ Selectionsortfinalletstouchglobals.asm
@@ -3,8 +3,8 @@
             
         .data
         .align  2
-valuecount:  .word   12
-values: .word   10, 1, 42, 99, 88, 46, 54, 156, -22, 0, 666, 1
+valuecount:  .word   3
+values: .word   10,-1,-5,-3
 fpromt: .asciiz "\n"
 
 
@@ -20,41 +20,48 @@ main:
 	move $s1, $0 #j = 0
 	move	$s3,$0		    #temp variable to hold min.
 	move 	$s4,$0	  	    #momory location that will hold the location of min
+	move $s6, $0
+	
 	
         la      $s7, valuecount     # get arraySize addr
         lw      $s2, 0($s7)         # s2: number of values in the array N 
         la      $s7, values         # get array addr
         lw	$s5,0($s7) #min temp 
-	
+	lw      $s3,0($s7)
 	#print#
-	jal printlist
+	#j printlist
 	
 	
 	
-	move $s0,$0 
-	la $s7,values 
-
+#	move $s0,$0 
+#	la $s7,values 
+	
+ 
+ 
 	
 	#call to start the function
 	#
-	move $a0,$s0, 
-	move $a1, $s2
-	li $a2, $s7 
+	#move $a0,$s0, 
+	#move $a1, $s2
+	#move $a2, $s7 
+	
+	
 
-	jal selectionsort
+	j loop
 
  	
             
 	
 	
 	#Print#
-	la $s7,values
-	move $s0,$0
-	jal printlist
+	#la $s7,values
+	#move $s0,$0
+	#jal printlist
 	
-	
-	li $v0, 10
-	syscall 
+	#move $a0, $t0 
+	#li $v0,1 
+	#syscall 
+
 
 	
 printlist: 	
@@ -75,42 +82,115 @@ printlist:
         j   printlist		    #loop to the next value
         
         
-done: jr $ra
+done: #jr $ra
+	#move $a0, $s4
+	#li $v0 1
+	#syscall 
 	
-         
-           
- swap: 
- 
-      
-           
- selectionsort: 
-		
-	bge     $s0, $s2, done2      # while (index < n)
-        addi    $s0, $s0, 1         # index++
-        
-         
-          #load parameters
-          jal findmin 
-          
-           
-            #Load parameters
-         jal swap  
-	
-	j selectionsort
-
-  done2: 
- 	jal printlist
- 	 
- 	 
- 	 
 	li $v0, 10
 	syscall 
-           
- findmin: 
+         
+loop: 
+	bge     $s1, $s2, inci      # while (index < n)
+        lw      $t0, 0($s7)         # t0 = next value in list
+        addi    $s1, $s1, 1         # index++
+        				
+        addi    $s7, $s7, 4         # update address pointer
+       # add	$a0,$s4,$0
+       # li	$v0,1
+       # syscall
+        
+        blt   	$t0, $s3, newmin    #if the new value is < t3
+        j       loop	    #continue looping through the array.
  
+                      
+inci: 
+	
+	
+	addi $s0,$s0,1
+	
+	
+	
+	move $a0, $s2
+	move $a1, $s4
+	move $a2,$s0
+
+	jal swap 
+	move $s1, $0
+	add $s1,$s0,$0
+	la $s7, values
+	mul $t5,$s0,4
+	add $s7, $s7, $t5
+	lw $s3, 0($s7) 
+	add $s4,$s0,$0
+	move $t0,$0
+	
+	
+	
+	bne $s0, $s2, loop 
+	la $s7,values
+	move $s0, $0 
+	j printlist 
+
+                                          
+swap: 
+	move $t1, $a0
+	move $t2, $a1 #new 
+	move $t8, $a2 #old
+	
+#	move $a0,$0
+#	move $a0,$a1
+#	li $v0, 1
+#	syscall
+	
+	
+	la $t6,values 
+	mul $t8, $a2,0
+	mul $t5, $a1, 4
+	add $t4,$0,$t5
+	
+	add $t6,$t6,$t3
+	lw $t7, 0($t6) 
+	
+	la $t6, values
+	add $t6,$t6,$t5
+	 
+	lw $t9, 0($t6)
+
+	
+	#move $t0,$t2	li $t0,4
+	li $t0, 0
+	
+	sw $t7,values($t5) #needs to hit the first position in the array 
+	sw $t9,values($t8)
+#	sw   $t9,values($t3)
+#	sw   $t7,values($t2)	
+	
+	#sw   $t8,0($t6)
+	
+	jr $ra
+	
+	
+
+
  
- newmin:
-  
+newmin: add $t2,$s3,$0 
+	move $s3,$t0 
+	add $s4, $s1,$0
+	
+	 
+	
+	j loop 
+	
+	
+		
+
+
+
+	
+
+
+
            
            
            
